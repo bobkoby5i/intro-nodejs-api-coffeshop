6e4a86415dbee8d0eaf906f176aae72c5796ae6a
koby5i <bobkoby5i@xdeq.com>
2022-07-02 22:46:31 +0200

    API CRUD - simple mocked respons

--- src/api/staff.js
+++ src/api/staff.js
@@ -10,4 +10,42 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+  console.log(`GET STAFF ${req.params.id}`);
+
+  res.json({
+    _id: '1',
+    firstName: 'Jan',
+    lastName: 'Kowalski',
+    startedAt: new Date(),
+    rating: 4.5,
+    position: 'waiter',
+    monthlySalary: 4000.0,
+  });
+});
+
+router.post('/:id', (req, res) => {
+  console.log('POST STAFF', req.body);
+
+  res.json({
+    ok: true,
+  });
+});
+
+router.put('/:id?', (req, res) => {
+  console.log(`PUT STAFF ${req.params.id}`, req.body);
+
+  res.json({
+    ok: true,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  console.log(`DELETE STAFF ${req.params.id}`);
+
+  res.json({
+    ok: true,
+  });
+});
+
 export default router;
