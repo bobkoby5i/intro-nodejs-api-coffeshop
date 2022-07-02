fd5afe6f1bf2ffd4f6925dee8c71915d978d41a7
koby5i <bobkoby5i@xdeq.com>
2022-07-02 22:16:26 +0200

    podstawowy erver i api endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import morgan from 'morgan';
+// import morgan from 'morgan';
+// import readFile  from "fs";
+// import path from "path";
+// import fs from 'fs';
+// import debug from 'debug';
 
-console.log('Hello World!');
+// const debugApp = debug('app');
+
+import { APP_PORT } from './config/app';
+import api from './api';
+
+(async function runApp() {
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  // app.use(express.json({extended: false})); // tu use with json
+  // app.use(express.urlencoded({ extended: true })); // to use with x-www-form-urlencoded.
+  console.log('Hello World!');
+
+  // Add routing
+  app.use(api);
+
+  const server = app.listen(APP_PORT, () => {
+    console.log(`Listening on port ${server.address().port}`);
+  });
+
+  // app.listen(APP_PORT, function server() {
+  //    console.log(`Listening on port ${APP_PORT}`);
+  // });
+})();
